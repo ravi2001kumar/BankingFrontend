a4f857c75b8416a31fd8fe252e7a14736cae94f2
ravi2001kumar <ravi.dusi2001@gmail.com>
2023-02-23 22:47:25 +0530

    updatetransaction

--- src/app/components/transaction/transaction.component.ts
+++ src/app/components/transaction/transaction.component.ts
@@ -4,6 +4,8 @@ import { emitDistinctChangesOnlyDefaultValue } from '@angular/compiler';
 import { TransactionService } from 'src/app/service/transaction.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DatePipe } from '@angular/common';
+import { CDetailsService } from 'src/app/service/cdetails.service';
+import { LoginService } from 'src/app/service/login.service';
 
 @Component({
   selector: 'app-transaction',
@@ -12,11 +14,42 @@ import { DatePipe } from '@angular/common';
 })
 export class TransactionComponent implements OnInit {
 
-  constructor(private detail:TransactionService,private snack:MatSnackBar,public datepipe: DatePipe ){}
+  constructor(private detail:TransactionService,private snack:MatSnackBar,public datepipe: DatePipe, private cdetail: CDetailsService, private login:LoginService ){}
   currentDate:any=this.datepipe.transform((new Date), 'MM/dd/yyyy h:mm:ss')
   ngOnInit(): void{
 
     this.data.date=this.currentDate
+
+    this.login.getUsername().subscribe(
+      {
+        next:(r:any)=>{
+          this.login.getDetails(r.username).subscribe(
+            {
+              next:(a:any)=>{
+                this.cdetail.getCustomerByUser(a).subscribe(
+                  {
+                    next:(b:any)=>{
+                      this.data.accountFrom=b.accountnumber;
+                    },
+                    error:(e)=>{
+                      console.log(e);
+                    }
+                  }
+                )
+              },
+              error:(e)=>{
+                this.snack.open(e.error.errorMessage,'close')
+              }
+            }
+          )
+          
+        },
+        error:(e)=>{
+          this.snack.open(e.error.errorMessage,'close')
+        }
+      }
+    )
+
     // this.detail.findTransaction(this.data).subscribe(
     //   {
     //     next:(r:any)=>{
